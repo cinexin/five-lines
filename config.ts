977567602ab09a61c8ffe1bc26b0dfd5a11014f3
cineexin <migueguillamon@gmail.com>
2022-04-02 12:21:28 +0200

    - Rename RawTile2 to RawTile - Remove unused things

--- config.ts
+++ config.ts
@@ -1,27 +1,29 @@
+import {RawTile} from "./tiles/raw-tiles/raw-tile";
+
 export const TILE_SIZE = 30;
 export const FPS = 30;
 export const SLEEP = 1000 / FPS;
 
-export enum RawTile {
-    AIR,
-    FLUX,
-    UNBREAKABLE,
-    PLAYER,
-    STONE,
-    FALLING_STONE,
-    BOX,
-    FALLING_BOX,
-    KEY1,
-    LOCK1,
-    KEY2,
-    LOCK2
-}
-
-export const rawMap: RawTile[][] = [
+export const rawMap: number[][] = [
     [2, 2, 2, 2, 2, 2, 2, 2],
     [2, 3, 0, 1, 1, 2, 0, 2],
     [2, 4, 2, 6, 1, 2, 0, 2],
     [2, 8, 4, 1, 1, 2, 0, 2],
     [2, 4, 1, 1, 1, 9, 0, 2],
     [2, 2, 2, 2, 2, 2, 2, 2],
+];
+
+export const RAW_TILES = [
+    RawTile.AIR,
+    RawTile.FLUX,
+    RawTile.UNBREAKABLE,
+    RawTile.PLAYER,
+    RawTile.STONE,
+    RawTile.FALLING_STONE,
+    RawTile.BOX,
+    RawTile.FALLING_BOX,
+    RawTile.KEY1,
+    RawTile.LOCK1,
+    RawTile.KEY2,
+    RawTile.LOCK2
 ];
--- map.ts
+++ map.ts
@@ -1,59 +1,34 @@
 import {iTile} from "./tiles/iTile";
 import {Air} from "./tiles/air";
 import {PlayerTile} from "./tiles/player-tile";
-import {Unbreakable} from "./tiles/unbreakable";
-import {Stone} from "./tiles/stone";
-import {Box} from "./tiles/box";
-import {Flux} from "./tiles/flux";
-import {Stopped} from "./tiles/state/stopped";
-import {Falling} from "./tiles/state/falling";
-import {Key} from "./tiles/key";
 import {RemoveLock1} from "./tiles/remove-lock-1";
-import {Lock} from "./tiles/lock";
 import {RemoveLock2} from "./tiles/remove-lock-2";
 import {KeyConfiguration} from "./tiles/key-configuration";
-import {RawTile} from "./config";
+import {RAW_TILES} from "./config";
 import {IFallingState} from "./tiles/state/iFalling-state";
 import {IRemoveStrategy} from "./tiles/iRemove-strategy";
 import {Player} from "./player";
 import {Position} from "./position";
+import {RawTile} from "./tiles/raw-tiles/raw-tile";
 
-function assertExhausted(x: never): never {
-    throw new Error("Unexpected object: " + x);
-}
+export const YELLOW_KEY = new KeyConfiguration("#ffcc00", true, new RemoveLock1());
+export const BLUE_KEY = new KeyConfiguration("#00ccff", false, new RemoveLock2());
 
-const YELLOW_KEY = new KeyConfiguration("#ffcc00", true, new RemoveLock1());
-const BLUE_KEY = new KeyConfiguration("#00ccff", false, new RemoveLock2());
-
-function transformTile(tile: RawTile) {
-    switch (tile) {
-        case RawTile.AIR: return new Air();
-        case RawTile.PLAYER: return new PlayerTile();
-        case RawTile.UNBREAKABLE: return new Unbreakable();
-        case RawTile.STONE: return new Stone(new Stopped());
-        case RawTile.FALLING_STONE: return new Stone(new Falling());
-        case RawTile.BOX: return new Box(new Stopped());
-        case RawTile.FALLING_BOX: return new Box(new Falling());
-        case RawTile.FLUX: return new Flux();
-        case RawTile.KEY1: return new Key(YELLOW_KEY);
-        case RawTile.LOCK1: return new Lock(YELLOW_KEY);
-        case RawTile.KEY2: return new Key(BLUE_KEY);
-        case RawTile.LOCK2: return new Lock(BLUE_KEY);
-        default: assertExhausted(tile);
-    }
+function transformTile(tile: RawTile): iTile {
+    return tile.transform();
 }
 
 export class Map {
     private readonly map;
 
     constructor(
-        rawMap: RawTile[][]
+        rawMap: number[][]
     ) {
         this.map = new Array(rawMap.length);
         for (let y = 0; y < rawMap.length; y++) {
             this.map[y] = new Array(rawMap[y].length);
             for (let x = 0; x < rawMap[y].length; x++) {
-                this.map[y][x] = transformTile(rawMap[y][x]);
+                this.map[y][x] = transformTile(RAW_TILES[rawMap[y][x]]);
             }
         }
     }
